7d029b82e4d3432ce0b825b0ec0e1bbae761fe84
Ubuntu <ubuntu@ip-172-31-21-74.eu-west-1.compute.internal>
2025-11-07 13:30:50 +0000

    created staging

---
+++ ecosystem.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: 'server.js',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 8080,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 8080,
+      },
+      watch: false,
+    },
+  ],  
+
+deploy: {
+    development: {
+      user: 'ubuntu',
+      host: '54.74.21.95',
+      ref: 'origin/staging',
+      repo: 'git@github.com:calculus-guy/Learning247-creators-platform-backend.git',
+      path: '/home/ubuntu/backend',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env development',
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+    production: {
+      user: 'ubuntu',
+      host: '34.251.237.6',
+      ref: 'origin/master',
+      repo: 'git@github.com:calculus-guy/Learning247-creators-platform-backend.git',
+      path: '/home/ubuntu/backend',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  },
+};
