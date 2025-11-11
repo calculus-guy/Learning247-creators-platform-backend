7debda4fb1fc9d9c71ed95fe84f5f5a689f5585a
ovdickson <dickson@cloudplexo.com>
2025-11-11 12:43:59 +0100

    updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,6 @@ module.exports = {
     {
       name: 'backend',
       script: 'server.js',
-      env: {
-        NODE_ENV: 'development',
-        PORT: 8080,
-      },
       env_development: {
         NODE_ENV: 'development',
         PORT: 8080,
