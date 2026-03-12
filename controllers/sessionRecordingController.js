ef153393c822a0e5026195b95161442c1d891223
calculus guy <sakariyauabdullateef993@gmail.com>
2026-03-12 12:14:32 +0100

    feat: series recording endpoint

--- controllers/sessionRecordingController.js
+++ controllers/sessionRecordingController.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const SessionRecordingSend = require('../models/SessionRecordingSend');
 const { sendSessionRecordingEmail } = require('../utils/email');
 const { Op } = require('sequelize');
-const { sequelize } = require('../config/db');
+const sequelize = require('../config/db');
 const { v4: uuidv4 } = require('uuid');
 
 /**
