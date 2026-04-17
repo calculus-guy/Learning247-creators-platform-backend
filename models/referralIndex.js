dee3b9dc1d1ff91fd3d3ed55154803bbb5c846ba
calculus guy <sakariyauabdullateef993@gmail.com>
2026-04-17 21:00:38 +0100

    fix : referral system

--- models/referralIndex.js
+++ models/referralIndex.js
@@ -1,64 +1,38 @@
 /**
  * Referral Models Index
- * 
- * Sets up associations between Referral models
+ * Sets up associations for the revamped referral system.
  */
 
 const ReferralCode = require('./ReferralCode');
 const ReferralCommission = require('./ReferralCommission');
+const UserReferral = require('./UserReferral');
 const User = require('./User');
 const Purchase = require('./Purchase');
 
 // ReferralCode associations
-ReferralCode.belongsTo(User, {
-  foreignKey: 'userId',
-  as: 'user'
-});
-
+ReferralCode.belongsTo(User, { foreignKey: 'partnerUserId', as: 'partner', constraints: false });
+ReferralCode.belongsTo(User, { foreignKey: 'createdBy', as: 'createdByUser', constraints: false });
+ReferralCode.hasMany(UserReferral, { foreignKey: 'referralCodeId', as: 'userReferrals' });
 ReferralCode.hasMany(ReferralCommission, {
   foreignKey: 'referralCode',
   sourceKey: 'referralCode',
   as: 'commissions'
 });
 
+// UserReferral associations
+UserReferral.belongsTo(User, { foreignKey: 'creatorUserId', as: 'creator', constraints: false });
+UserReferral.belongsTo(User, { foreignKey: 'partnerUserId', as: 'partner', constraints: false });
+UserReferral.belongsTo(ReferralCode, { foreignKey: 'referralCodeId', as: 'code' });
+
 // ReferralCommission associations
-ReferralCommission.belongsTo(User, {
-  foreignKey: 'referrerUserId',
-  as: 'referrer'
-});
+ReferralCommission.belongsTo(User, { foreignKey: 'referrerUserId', as: 'referrer', constraints: false });
+ReferralCommission.belongsTo(User, { foreignKey: 'refereeUserId', as: 'referee', constraints: false });
+ReferralCommission.belongsTo(Purchase, { foreignKey: 'purchaseId', as: 'purchase' });
 
-ReferralCommission.belongsTo(User, {
-  foreignKey: 'refereeUserId',
-  as: 'referee'
-});
+// User reverse associations
+User.hasMany(ReferralCode, { foreignKey: 'partnerUserId', as: 'referralCodes', constraints: false });
+User.hasMany(UserReferral, { foreignKey: 'creatorUserId', as: 'userReferral', constraints: false });
+User.hasMany(ReferralCommission, { foreignKey: 'referrerUserId', as: 'referralsMade', constraints: false });
+User.hasMany(ReferralCommission, { foreignKey: 'refereeUserId', as: 'referralsReceived', constraints: false });
 
-ReferralCommission.belongsTo(User, {
-  foreignKey: 'approvedBy',
-  as: 'approver'
-});
-
-ReferralCommission.belongsTo(Purchase, {
-  foreignKey: 'purchaseId',
-  as: 'purchase'
-});
-
-// User associations
-User.hasMany(ReferralCode, {
-  foreignKey: 'userId',
-  as: 'referralCodes'
-});
-
-User.hasMany(ReferralCommission, {
-  foreignKey: 'referrerUserId',
-  as: 'referralsMade'
-});
-
-User.hasMany(ReferralCommission, {
-  foreignKey: 'refereeUserId',
-  as: 'referralsReceived'
-});
-
-module.exports = {
-  ReferralCode,
-  ReferralCommission
-};
+module.exports = { ReferralCode, ReferralCommission, UserReferral };
